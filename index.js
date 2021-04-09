449ae47a5dbca99a4fe1f2f77ebd04c8229f555b
pathum kalhan <pathumsimpson@gmail.com>
2021-04-09 13:32:48 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = async function (context, req) {
+  context.log("JavaScript HTTP trigger function processed a request.");
+
+  const name = req.query.name || (req.body && req.body.name);
+  const responseMessage = "P@hum Kalhan";
+
+  context.res = {
+    // status: 200, /* Defaults to 200 */
+    body: responseMessage,
+  };
+};
